ca41a299ad56d6e46907e3884281e335258cd212
Bart Heemskerk <de.uil.met.een@gmail.com>
2016-05-13 17:55:43 +0200

    ugly fix outlet problem

--- app/router.js
+++ app/router.js
@@ -6,7 +6,7 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('index', { path: '/' }, function() {
+  this.route('index', {path:'/'}, function() {
     this.route('mygoals');
     this.route('trending');
     this.route('newgoals');
