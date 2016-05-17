8291478d06bbe7da21333dd18142757d0d4fd67d
Steffan Sluis <steffansluis@gmail.com>
2016-05-17 12:41:53 +0200

    Update mdl

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,6 +3,9 @@
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
+  var environment = process.env.EMBER_ENV;
+  var isProduction = ['staging', 'production'].indexOf(environment) >= 0;
+
   var app = new EmberApp(defaults, {
     dotEnv: {
       clientAllowedKeys: [
@@ -16,21 +19,60 @@ module.exports = function(defaults) {
         'bower_components/material-design-lite/src',
         'bower_components/roboto-fontface/css'
       ]
-    }
+    },
+    minifyJS: {
+      enabled: isProduction
+    },
+    minifyCSS: {
+      enabled: isProduction
+    },
+    sourcemaps: {
+      enabled: !isProduction
+    },
+    autoprefixer: {
+      enabled: isProduction
+    },
+    fingerprint: {
+      enabled: isProduction,
+      exclude: [
+        'favicon.ico',
+      ],
+      extensions: [
+        'js',
+        'css',
+        'png',
+        'map',
+        'svg',
+        'mp4',
+        'eot',
+        'ttf',
+        'woff',
+        'woff2',
+        'json'
+      ]
+    },
+    jscsOptions: {
+      testGenerator: testGenerator
+    },
+    tests: !isProduction,
+    hinting: !isProduction
   });
 
-  // Use `app.import` to add additional libraries to the generated
-  // output files.
-  //
-  // If you need to use different assets in different
-  // environments, specify an object as the first parameter. That
-  // object's keys should be the environment name and the values
-  // should be the asset to use in that environment.
-  //
-  // If the library that you are including contains AMD or ES6
-  // modules that you would like to import into your application
-  // please specify an object with the list of modules as keys
-  // along with the exports of each module as its value.
+  function testGenerator(relativePath, errors) {
+    if (errors) {
+      errors = this.escapeErrorString('\n' + errors);
+    }
+
+    return [
+      'describe("JSCS - ' + relativePath + '", function () {',
+        'it("should pass jscs", function () {',
+          'expect(' + !errors + ', "' + relativePath + ' should pass JSCS' + errors + '").to.be.ok;',
+        '});',
+      '});'
+    ].join('\n');
+  }
+
+  app.import(app.bowerDirectory + '/prettysize-bower/prettysize.js');
 
   return app.toTree();
 };
