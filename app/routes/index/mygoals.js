5eb4981ad10deb90b0d474823b42d96e9ceff19c
Bart Heemskerk <de.uil.met.een@gmail.com>
2016-05-19 15:44:27 +0200

    first communication with api

--- app/routes/index/mygoals.js
+++ app/routes/index/mygoals.js
@@ -1,5 +1,6 @@
 import Ember from 'ember';
 import AuthenticatedRouteMixin from 'feedbackfruits-accounts/mixins/authenticated-route-mixin';
+import mockMaker from '../../utils/mockMaker';
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
   model(){
