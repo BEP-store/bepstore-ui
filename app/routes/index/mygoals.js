2a975e587b15645de6ebca648406a29d403b0c4e
Bart Heemskerk <de.uil.met.een@gmail.com>
2016-05-23 12:47:00 +0200

    fixed nonsense in getting goals

--- app/routes/index/mygoals.js
+++ app/routes/index/mygoals.js
@@ -1,10 +1,8 @@
 import Ember from 'ember';
 import AuthenticatedRouteMixin from 'feedbackfruits-accounts/mixins/authenticated-route-mixin';
-import mockMaker from '../../utils/mockMaker';
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
   model(){
-    return this.store.findAll('user');
-    //return mockMaker().multipleGoals();
+    return this.get('store').findAll('activity');
   }
 });
