4d0c773a4087cc2ff17d2b3d8cde78aa052c4a02
Bart Heemskerk <de.uil.met.een@gmail.com>
2016-05-24 12:02:00 +0200

    processed feedback

--- app/routes/index/mygoals.js
+++ app/routes/index/mygoals.js
@@ -1,20 +1,8 @@
 import Ember from 'ember';
 import AuthenticatedRouteMixin from 'feedbackfruits-accounts/mixins/authenticated-route-mixin';
 
-//const { inject: { service } } = Ember;
-
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
-//  account: service(),
   model(){
-  /*
-    this.get('store').queryRecord('user', { filter: { account: this.get('account').get('id') } }).then(function(user){
-      return Ember.RSVP.hash({
-        pending: user.get('pending_goals'),
-        building: user.get('building_goals'),
-        finished: user.get('finished_goals'),
-      });
-    });
-  */
     return this.get('store').findAll('goal');
   }
 });
