34354b119501af58358f1a382f0e0072457acab4
Bart Heemskerk <de.uil.met.een@gmail.com>
2016-06-04 23:51:56 +0200

    First, we build a wall that seperates the goals by status

--- app/routes/index/mygoals.js
+++ app/routes/index/mygoals.js
@@ -2,7 +2,34 @@ import Ember from 'ember';
 import AuthenticatedRouteMixin from 'feedbackfruits-accounts/mixins/routes/authenticated-route-mixin';
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
+  working: null,
+  pending: null,
+  done: null,
+
   model(){
-    return this.get('store').findAll('goal');
+    this.set('working',[]);
+    this.set('pending',[]);
+    this.set('done', []);
+
+    let I = this.get('store').findRecord('user','current');
+    this.get('store').findAll('goal').then((goals) => {
+      goals.forEach(function(item) {
+        item.get('contributors').then((contributors) => {
+          if(contributors.isAny('id', I.get('id'))){
+            let status = item.get('status');
+            if(status === 'init' || status === 'spec-design'){
+              alert(item.get('title'));
+              this.get('pending').pushObject(item);
+            }
+            else if (status === 'building' || status === 'testing') {
+              this.get('working').pushObject(item);
+            }
+            else if (status === 'finished') {
+              this.get('done').pushObject(item);
+            }
+          }
+        });
+      });
+    });
   }
 });
