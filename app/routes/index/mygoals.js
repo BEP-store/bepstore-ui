e6f3474694e9c5d64c4c76af96f7c06eafd844b1
Bart <de.uil.met.een@gmail.com>
2016-06-07 15:06:08 +0200

    make MyGoals great again (#51)
    * First, we build a wall that seperates the goals by status
    
    * When my PR is merged, no bad goals will enter myGoals ever again
    
    * failing tests and jshint? I can't remeber having those, I have the best code!
    
    * Some code I did not write was making it ugly, so I removed it
    
    * I did not refactor, I did what I always planned to do

--- app/routes/index/mygoals.js
+++ app/routes/index/mygoals.js
@@ -2,7 +2,20 @@ import Ember from 'ember';
 import AuthenticatedRouteMixin from 'feedbackfruits-accounts/mixins/routes/authenticated-route-mixin';
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
+
   model(){
-    return this.get('store').findAll('goal');
+    /* jshint ignore:start */
+    return this.get('store').findRecord('user','current')
+      .then(I => {
+        let mapFn = (goal) => {
+          return [goal, goal.get('contributors').isAny('id', I.get('id'))];
+        };
+        return this.get('store').findAll('goal').then((goals) => {
+          return Promise.all(goals.map(mapFn));
+        }).then((mygoals) => {
+          return mygoals.filter(([goal, isMine]) => isMine).map(([goal, isMine]) => goal);
+        });
+      });
+      /* jshint ignore:end */
   }
 });
