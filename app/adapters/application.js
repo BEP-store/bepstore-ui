0d97a1b689d9478fdae8932bf24e65892b8e7790
Wouter Kooyman van Guldener <wouter@wick-it.nl>
2016-06-10 10:51:29 +0200

    Switched to JSONAPI.

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,8 +1,8 @@
-import ActiveModelAdapter from 'active-model-adapter';
+import JSONAPIAdapter from 'ember-data/adapters/json-api';
 import DataAdapterMixin from 'feedbackfruits-accounts/mixins/adapters/data-adapter-mixin';
-import config from '../config/environment';
+import config from 'bepstore/config/environment';
 
-export default ActiveModelAdapter.extend(DataAdapterMixin, {
+export default JSONAPIAdapter.extend(DataAdapterMixin, {
   host: config.host,
   namespace: config.namespace,
   coalesceFindRequests: true,
