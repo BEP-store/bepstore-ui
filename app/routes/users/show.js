7f43d96d51c6dd98f1284062a8f688a40060d094
Bart Heemskerk <de.uil.met.een@gmail.com>
2016-06-21 14:24:19 +0200

    refix the fix fix

--- app/routes/users/show.js
+++ app/routes/users/show.js
@@ -3,6 +3,6 @@ import ceesHerpdiederpston from 'bepstore/utils/ceesHerpdiederpston';
 
 export default Ember.Route.extend({
   model(params) {
-    return this.get('store').findRecord('user', params.user_id) || ceesHerpdiederpston().create();
+    return this.get('store').findRecord('user', params.user_id).catch((error) => ceesHerpdiederpston().create()); 
   }
 });
