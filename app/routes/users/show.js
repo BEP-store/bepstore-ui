d2eadba6141149cf6e5d97f05b3f0498c2fddacf
Bart <de.uil.met.een@gmail.com>
2016-06-21 14:47:50 +0200

    fixed not-loading user-profile (#78)
    * fixed not-loading user-profile
    
    * refix the fix fix
    
    * jshint fix the fix

--- app/routes/users/show.js
+++ app/routes/users/show.js
@@ -3,10 +3,6 @@ import ceesHerpdiederpston from 'bepstore/utils/ceesHerpdiederpston';
 
 export default Ember.Route.extend({
   model(params) {
-    if(params.user_id === 'current' || this.get('store').peekRecord('user', params.user_id)){
-      return this.get('store').findRecord('user', params.user_id);
-    } else {
-      return ceesHerpdiederpston().create();
-    }
+    return this.get('store').findRecord('user', params.user_id).catch(() => ceesHerpdiederpston().create()); 
   }
 });
